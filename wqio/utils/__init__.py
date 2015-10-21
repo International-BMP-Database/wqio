b6b4d61140c970b1e67b716f2bf37eeec7a19521
Paul Hobson <pmhobson@gmail.com>
2015-10-21 15:36:48 -0700

    update imports for reportutils module

--- wqio/utils/__init__.py
+++ wqio/utils/__init__.py
@@ -3,6 +3,7 @@ from matplotlib.scale import register_scale
 from .misc import *
 from .dateutils import *
 from .numutils import *
+from .reportutils import *
 from .probscale import ProbScale
 from . import figutils
 
