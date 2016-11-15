e785a3504b6cb49d13c9d4caa811abe2166ab38c
Paul Hobson <pmhobson@gmail.com>
2016-11-15 11:20:06 -0800

    .dc

--- examples/plot_color_palette.py
+++ examples/plot_color_palette.py
@@ -1,38 +1,94 @@
 """
-Color palette choices
-=====================
+Basic of a DataCollection
+=========================
 
-_thumb: 0.6, 0.4
+This examples show how to create a ``DataCollection``, access some
+of its statistics, and pipe the data into seaborn plotting functions.
+
 """
 
-import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
-sns.set(style="white", context="talk")
-rs = np.random.RandomState(7)
+import numpy
+import seaborn
+from matplotlib import pyplot
 
-# Set up the matplotlib figure
-f, (ax1, ax2, ax3) = plt.subplots(3, 1, figsize=(8, 6), sharex=True)
+import wqio
+from wqio.tests import helpers
+from wqio.utils import head_tail
 
-# Generate some sequential data
-x = np.array(list("ABCDEFGHI"))
-y1 = np.arange(1, 10)
-sns.barplot(x, y1, palette="BuGn_d", ax=ax1)
-ax1.set_ylabel("Sequential")
+seaborn.set(style="ticks", context="talk")
 
-# Center the data to make it diverging
-y2 = y1 - 5
-sns.barplot(x, y2, palette="RdBu_r", ax=ax2)
-ax2.set_ylabel("Diverging")
 
-# Randomly reorder the data to make it qualitative
-y3 = rs.choice(y1, 9, replace=False)
-sns.barplot(x, y3, palette="Set3", ax=ax3)
-ax3.set_ylabel("Qualitative")
+#######################################
+# Load example data and show a few rows
 
-# Finalize the plot
-sns.despine(bottom=True)
-plt.setp(f.axes, yticks=[])
-plt.tight_layout(h_pad=3)
+df = helpers.make_dc_data_complex()
+print(head_tail(df))
 
-plt.show()
+
+#####################################################################
+# Create the data collection object, show some of the data attributes
+
+dc = wqio.DataCollection(df, rescol='res', qualcol='qual',
+                         stationcol='loc', paramcol='param',
+                         ndval='<', othergroups=['state'],
+                         pairgroups=['state', 'bmp'],
+                         useros=True, filterfxn=None,
+                         bsiter=10000)
+
+
+##################
+# ROS'd, tidy data
+
+head_tail(dc.tidy)
+
+
+########################
+# Non-ROS'd, paired data
+
+head_tail(dc.paired)
+
+
+############################
+# Show how much data we have
+head_tail(dc.count)
+
+
+#######################
+# Show some basic stats
+
+head_tail(dc.median)
+
+
+####################################
+# Show some hypothesis tests results
+
+head_tail(dc.wilcoxon)
+
+
+################################
+# Use seaborn to make some plots
+# This is basic boxplot
+
+fg = seaborn.factorplot(
+    x='state', y='ros_res', data=dc.tidy,
+    col='param', col_wrap=2, hue='loc', kind='box',
+    aspect=1.75, size=3, margin_titles=True,
+).set(yscale='log').set_ylabels('Concentration')
+
+
+#############################
+# This is a split violin plot.
+# The dashed lines show the quartiles
+
+fg = (
+    dc.tidy.assign(log_ros_res=numpy.log10(dc.tidy['ros_res']))
+      .pipe(
+            (seaborn.factorplot, 'data'),
+            x='state', y='log_ros_res',
+            col='param', col_wrap=2,
+            hue='loc', hue_order=['Inflow', 'Outflow'],
+            kind='violin', split=True, inner='quartile',
+            aspect=1.75, size=3, margin_titles=True,
+            linewidth=1.0
+      ).set_ylabels('Log10 of Concentration')
+)
