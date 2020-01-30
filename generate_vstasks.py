136ba9c6372b1630ffe9707d92ce7fce21d613f1
Paul Hobson <pmhobson@gmail.com>
2020-01-30 10:24:00 -0800

    Update the task generator to v2.0.0 [ci skip]

--- generate_vstasks.py
+++ generate_vstasks.py
@@ -4,12 +4,10 @@ from pathlib import Path
 
 TEMPLATE = """\
 {{
-    "version": "0.1.0",
-    "isShellCommand": false,
+    "version": "2.0.0",
     "args": [],
-    "showOutput": "always",
     "echoCommand": false,
-    "suppressTaskName": false,
+    "runner": "terminal",
     "tasks": [
         {{
             "label": "format-code",
@@ -20,18 +18,56 @@ TEMPLATE = """\
                 "{modulename:s}/*.py",
                 "{modulename:s}/*/*.py",
             ],
-        }}
+        }},
         {{
-            "taskName": "test",
+            "label": "test",
+            "type": "shell",
             "command": "{pyexec:s}",
             "args": [
                 "check_{modulename:s}.py",
-                "--cov",
-                "--pep8"
-            ]
+                "--tb=short",
+                "--strict",
+                "--cov-report=xml:cov.xml",
+                "--cov={modulename:s}"
+            ],
+            "group": {{
+                "kind": "test",
+                "isDefault": true
+            }}
         }},
         {{
-            "taskName": "notebooks",
+            "label": "test-lastfailed",
+            "command": "{pyexec:s}",
+            "args": [
+                "check_{modulename:s}.py",
+                "--strict",
+                "--tb=short",
+                "--lf",
+            ],
+            "group": {{
+                "kind": "test",
+                "isDefault": true
+            }},
+            "type": "shell"
+        }},
+        {{
+            "label": "test-debuggin",
+            "command": "{pyexec:s}",
+            "args": [
+                "check_{modulename:s}.py",
+                "--strict",
+                "--tb=short",
+                "--lf",
+                "--pdb"
+            ],
+            "group": {{
+                "kind": "test",
+                "isDefault": true
+            }},
+            "type": "shell"
+        }},
+        {{
+            "label": "notebooks",
             "options": {{
                 "cwd": "${{workspaceRoot}}/docs/tutorial"
             }},
@@ -39,7 +75,7 @@ TEMPLATE = """\
             "args": ["make.py"]
         }},
         {{
-            "taskName": "docs",
+            "label": "docs",
             "options": {{
                 "cwd": "${{workspaceRoot}}/docs"
             }},
@@ -53,16 +89,15 @@ TEMPLATE = """\
 
 if __name__ == "__main__":
     configdir = Path(".vscode")
-    configdir.mkdir(exists_ok=True, parents=True)
+    configdir.mkdir(exist_ok=True, parents=True)
     configpath = configdir / "tasks.json"
 
+    python = str(Path(sys.executable))
     if len(sys.argv) < 2:
         name = Path.cwd().name
     else:
         name = sys.argv[1]
 
-    python = str(Path(sys.executable))
     config = TEMPLATE.format(pyexec=python, modulename=name)
-
     with configpath.open("w") as configfile:
         configfile.write(config)
